0f5cc279af8d1e7bff2462cda9dc168dc42d5095
2l47 <2l47@protonmail.com>
2022-12-17 16:20:48 -0600

    Remove old scripts

--- remove-pulseaudio-sinks.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Log the commands run
-set -x
-
-# I'm not cleaning up this line.
-pactl list sinks | grep -P 'Name: (recording|combined)(\.\d+)?|Owner Module: \d+' | grep -E 'Name: (recording|combined)' -A 1 | grep -Po '(?<=Owner Module: )\d+' | xargs -n 1 pactl unload-module
