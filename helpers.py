d398185d7c8a096aaf103efae6c07f0562a763a6
2l47 <2l47@protonmail.com>
2022-12-18 17:03:49 -0600

    Move card switching into its own helper function

--- helpers.py
+++ helpers.py
@@ -61,3 +61,32 @@ def vr_running():
 		return subprocess.check_output("pgrep vrmonitor", shell=True)
 	except subprocess.CalledProcessError:
 		pass
+
+
+def handle_valve_index_card_switching():
+	# Switch the graphics card audio output from the monitor speaker to the Valve Index when VR is running
+	print(BLUE + "Configuring GPU DisplayPort audio output...")
+	if VALVE_INDEX_MIC in get_inputs() and vr_running():
+		print("Valve Index mic detected, checking card profile")
+		data = pactl("list sinks", " | grep -Po '(?<=Name: |Sample Specification: |device\.profile\.name = ).*'").split("\n")
+		found_dp_output = False
+		for index, item in enumerate(data):
+			if item.startswith("alsa_output.pci-0000_08_00.1."):
+				found_dp_output = True
+				sample_spec, profile = data[index + 1:index + 2 + 1] # Slice funniness
+				print(f"\tName: {item}")
+				print(f"\tSample spec: {sample_spec}")
+				print(f"\tProfile: {profile}")
+				if "float32le" in sample_spec or "192000Hz" in sample_spec:
+					# We have to switch card profiles in order to get the sample rate set correctly
+					print("Incompatible sample spec detected, switching card profiles")
+					pactl("set-card-profile 0 output:hdmi-surround71-extra3") # Digital Surround 7.1 (HDMI 4) Output"
+					time.sleep(2) # Wait a moment
+		if not found_dp_output:
+			print("Couldn't find the DP output; swapping card to HDMI monitor to fix")
+			pactl("set-card-profile 0 output:hdmi-stereo-extra4") # Digital Stereo (HDMI 5) Output"
+		# If the Valve Index is plugged in, switch GPU audio output to it
+		pactl("set-card-profile 0 output:hdmi-stereo-extra3") # Digital Stereo (HDMI 4) Output"
+	else:
+		# Otherwise, switch GPU audio output to the HDMI monitor
+		pactl("set-card-profile 0 output:hdmi-stereo-extra4") # "Digital Stereo (HDMI 5) Output"
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import pprint
 import time
 
-from helpers import add_sinks, get_outputs, get_inputs, pactl, remove_sinks, vr_running
+from helpers import add_sinks, get_outputs, get_inputs, handle_valve_index_card_switching, pactl, remove_sinks, vr_running
 
 
 
@@ -81,33 +81,6 @@ def set_input_device():
 	available_inputs = get_inputs()
 	print(f"Available input devices:\n{pprint.pformat(available_inputs)}")
 
-	# Switch the graphics card audio output from the monitor speaker to the Valve Index when VR is running
-	print(BLUE + "Configuring GPU DisplayPort audio output...")
-	if VALVE_INDEX_MIC in available_inputs and vr_running():
-		print("Valve Index mic detected, checking card profile")
-		data = pactl("list sinks", " | grep -Po '(?<=Name: |Sample Specification: |device\.profile\.name = ).*'").split("\n")
-		found_dp_output = False
-		for index, item in enumerate(data):
-			if item.startswith("alsa_output.pci-0000_08_00.1."):
-				found_dp_output = True
-				sample_spec, profile = data[index + 1:index + 2 + 1] # Slice funniness
-				print(f"\tName: {item}")
-				print(f"\tSample spec: {sample_spec}")
-				print(f"\tProfile: {profile}")
-				if "float32le" in sample_spec or "192000Hz" in sample_spec:
-					# We have to switch card profiles in order to get the sample rate set correctly
-					print("Incompatible sample spec detected, switching card profiles")
-					pactl("set-card-profile 0 output:hdmi-surround71-extra3") # Digital Surround 7.1 (HDMI 4) Output"
-					time.sleep(2) # Wait a moment
-		if not found_dp_output:
-			print("Couldn't find the DP output; swapping card to HDMI monitor to fix")
-			pactl("set-card-profile 0 output:hdmi-stereo-extra4") # Digital Stereo (HDMI 5) Output"
-		# If the Valve Index is plugged in, switch GPU audio output to it
-		pactl("set-card-profile 0 output:hdmi-stereo-extra3") # Digital Stereo (HDMI 4) Output"
-	else:
-		# Otherwise, switch GPU audio output to the HDMI monitor
-		pactl("set-card-profile 0 output:hdmi-stereo-extra4") # "Digital Stereo (HDMI 5) Output"
-
 	for priority in inputs:
 		if priority in available_inputs:
 			# Set this as the default source (input)
@@ -137,6 +110,9 @@ def handle_recording():
 
 # Main program logic - just run until terminated
 while True:
+	# Handles switching the GPU to the correct profile, ensuring that the output is actually available for the combined sink
+	handle_valve_index_card_switching()
+
 	set_output_device()
 	handle_recording()
 	print()
