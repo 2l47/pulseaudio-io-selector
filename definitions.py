a9ab301d363c221d48cfd7e161b477dac0924c4d
2l47 <2l47@protonmail.com>
2023-05-14 20:34:31 -0500

    Add airpods, increase BT connect interval from 1s to 15s

--- definitions.py
+++ definitions.py
@@ -5,6 +5,7 @@
 VALVE_INDEX_DP = "alsa_output.pci-0000_08_00.1.hdmi-stereo-extra3"
 VALVE_INDEX_MIC = "alsa_input.usb-Valve_Corporation_Valve_VR_Radio___HMD_Mic_46B7E4067C-LYM-01.multichannel-input"
 
+AIRPODS = "bluez_sink.58_0A_D4_E7_09_C5.a2dp_sink"
 BT_SPEAKER = "bluez_sink.FC_58_FA_73_8C_31.a2dp_sink"
 
 # List of outputs, in order of preference
@@ -12,6 +13,7 @@ BT_SPEAKER = "bluez_sink.FC_58_FA_73_8C_31.a2dp_sink"
 # pulseeffects, amplifier, HDMI monitor (or Valve Index), motherboard line out
 outputs = [
 	VALVE_INDEX_DP,
+	AIRPODS,
 	BT_SPEAKER,
 	"PulseEffects_apps",
 	"alsa_output.usb-MONOPRICE_MONOLITH_HPA-00.iec958-stereo",
@@ -34,7 +36,7 @@ inputs = [
 ]
 
 # Time between attempting to connect to Bluetooth devices.
-BT_CONNECT_INTERVAL = 1.000
+BT_CONNECT_INTERVAL = 15.000
 
 # Bluetooth devices to try to connect to
 bluetooth_devices = [
